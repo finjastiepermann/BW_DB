d2929e99e9a8b89d7ccd07b6b58e3b26d1b19621
finjastiepermann <finja.stiepermann@outlook.de>
2023-05-24 10:28:07 +0200

    Added hsqldb datasource

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,8 @@
-
+package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class Main {
 
